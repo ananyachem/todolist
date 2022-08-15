58a912dad6dd8d23aff8076100012c668e02ee25
ananyachem <108514240+ananyachem@users.noreply.github.com>
2022-08-15 18:20:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import {Heading, Input} from "@chakra-ui/react"
+
+
 
 function App() {
+  const [items, setItems]=React.useState([])
+  const [item, setItem] = React.useState("Add")
+  const handleChange = (e) => {
+    e.preventDefault();
+    setItem(e.target.value)
+  }
+  console.log(item);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Heading  bgGradient="linear(to-l, #7928CA, #FF0080)"
+  bgClip="text"
+  fontSize="6xl"
+  fontWeight="extrabold">Todo List</Heading>
+  <Input placeholder='Add Todo' htmlSize={20} width='auto' value={item} onChange={(e) => handleChange(e)} />
+</>
+
   );
 }
 
